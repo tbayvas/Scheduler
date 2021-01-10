193ec08b9667a33e0897ae6d50ee18b89247e4a8
Tony Bayvas <tbayvas@outlook.com>
2021-01-10 14:15:36 -0600

    Modularized code

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import CourseList from './components/CourseList';
   
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -27,25 +29,6 @@ const schedule = {
   ]
 };
 
-const getCourseNumber = course => (
-  course.id.slice(1)
-);
-
-const Course = ({ course }) => (
-  <TouchableOpacity style={styles.courseButton}>
-    <Text style={styles.courseText}>
-      {`CS ${getCourseNumber(course)}\n${course.meets}`}
-    </Text>
-  </TouchableOpacity>
-);
-
-const CourseList = ({ courses }) => (
-  <ScrollView>
-    <View style={styles.courseList}>
-      { courses.map(course => <Course key={course.id} course={ course } />) }
-    </View>
-  </ScrollView>
-);
 
 const Banner = ({ title }) => (
   <Text style={styles.banner}>{ title }</Text>
@@ -70,29 +53,6 @@ const styles = StyleSheet.create({
   banner: {
     color: '#888',
     fontSize: 32,
-  },
-  courseList: {
-    flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-  courseButton: {
-    borderRadius: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 10,
-    height: 60,
-    padding: 10,
-    minWidth: 90,
-    maxWidth: 90,
-    backgroundColor: '#66b0ff',
-  },
-  courseText:{
-    color: '#fff',
-    fontSize: 12,
-    textAlign: 'center',
   },
 });
 
