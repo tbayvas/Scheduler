659f0074ce25a25fbb29b5c8c387cb653697ff87
Tony Bayvas <tbayvas@outlook.com>
2021-01-27 14:01:53 -0600

    Database fetch

---
+++ firebaseApp.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkTiDiz3CkHBjqFQXlPYXnLItjAS66lp0",
+  authDomain: "scheduler-39cbb.firebaseapp.com",
+  databaseURL: "https://scheduler-39cbb-default-rtdb.firebaseio.com",
+  projectId: "scheduler-39cbb",
+  storageBucket: "scheduler-39cbb.appspot.com",
+  messagingSenderId: "612439951241",
+  appId: "1:612439951241:web:cd711a3f50dfc483f68358",
+  measurementId: "G-GG2XX8686C"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export { firebase };
--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -3,33 +3,35 @@ import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } fr
 import CourseList from '../components/CourseList';
 import UserContext from '../UserContext';
 import CourseEditScreen from './CourseEditScreen';
+import { firebase } from '../firebaseApp';
 
 
 const Banner = ({title}) => (
     <Text style={styles.bannerStyle}>{title || '[loading...]'}</Text>
   );
   
-  
+const fixCourses = json => ({
+    ...json,
+    courses: Object.values(json.courses)
+  });
+
 const ScheduleScreen = ( { navigation }) => {
   const user = useContext(UserContext);
   const canEdit = user && user.role === 'admin';
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
-
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
 
   const view = (course) => {
     navigation.navigate(canEdit ? 'CourseEditScreen' : 'CourseDetailScreen', { course });
   };
 
   useEffect(() => {
-    const fetchSchedule =  async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(json);
+    const db = firebase.database().ref();
+    const handleData = snap => {
+      if (snap.val()) setSchedule(fixCourses(snap.val()));
     }
-    fetchSchedule();
-  }, []);
+    db.on('value', handleData, error => alert(error));
+    return () => { db.off('value', handleData); };
+    }, []);
 
   return (
     <SafeAreaView style={styles.container}>
